6e27bab259b65d3a1d3081de3505693cc9a6026a
Issam Ounejjar <iounejjar@sqli.com>
2024-09-23 16:44:48 +0100

    add ft_strcmp_lowercase function

---
+++ string/ft_strcmp_lowercase.c
@@ -0,0 +1,23 @@
+#include "ft_string.h"
+
+int ft_strcmp_lowercase(const char *s1, const char *s2) {
+  int i = 0;
+
+  while (s1[i] != '\0') {
+    char a = ft_to_lower(s1[i]);
+    char b = ft_to_lower(s2[i]);
+
+    if (a != b)
+      return a - b;
+
+    i++;
+  }
+
+  char a = s1[i];
+  char b = ft_to_lower(s2[i]);
+
+  if (a != b)
+    return a - b;
+
+  return 0;
+}
--- string/ft_string.h
+++ string/ft_string.h
@@ -1,4 +1,5 @@
 #ifndef FT_STRING_H
 #define FT_STRING_H
 
+int ft_strcmp_lowercase(const char *s1, const char *s2);
 #endif
