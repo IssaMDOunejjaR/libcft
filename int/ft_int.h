20e7ba61e31bb0d3abfbb16148c18b49e97e7eea
IssaMDounejjaR <otaku6issam@gmail.com>
2024-09-28 13:44:53 +0100

    add some utilities function for int

---
+++ int/ft_int.h
@@ -0,0 +1,8 @@
+#ifndef FT_INT_H
+#define FT_INT_H
+
+#include <stddef.h>
+
+size_t ft_int_len(int number);
+
+#endif
---
+++ int/ft_int_len.c
@@ -0,0 +1,15 @@
+#include "ft_int.h"
+
+size_t ft_int_len(int number) {
+  size_t len = 0;
+
+  if (number <= 0)
+    len++;
+
+  while (number > 0) {
+    len++;
+    number /= 10;
+  }
+
+  return len;
+}
