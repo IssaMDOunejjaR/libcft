296fb9942c0c89b09c06fb5ccf78a5dc4fb6fbf2
IssaMDounejjaR <otaku6issam@gmail.com>
2024-10-06 21:03:10 +0100

    fix ft_strcat to copy only

--- string/ft_strcat.c
+++ string/ft_strcat.c
@@ -2,14 +2,8 @@
 
 char *ft_strcat(char *dst, const char *src) {
   size_t dst_len = ft_strlen(dst);
-  size_t str_len = (ft_strlen(src) + dst_len + 1);
 
-  char *str = malloc(sizeof(char) * str_len);
+  ft_strcpy(dst + dst_len, src);
 
-  ft_strcpy(str, dst);
-  ft_strcpy(str + dst_len, src);
-
-  str[str_len] = '\0';
-
-  return str;
+  return dst;
 }
